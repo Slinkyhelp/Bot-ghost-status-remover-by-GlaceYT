0d4be39eb0ddad52c70222df8c4effea65ef3612
Slinkyhelp <zeenatsha0@gmail.com>
2024-08-30 17:39:32 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Watching NerdMC","Playing Minecraft"];
 
 
 let currentIndex = 0;
